6301c1c8fbc4448bed4de6f877a3a0cbb6f3d4d5
shahadaljaeethin <shahadaljaeethin@gmail.com>
2025-10-30 10:12:24 +0300

    shape class

---
+++ src/Shape.java
@@ -0,0 +1,23 @@
+public abstract class Shape {
+private int x,y;
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public abstract double calculateArea();
+    public abstract double calculateCircumference();
+
+}
